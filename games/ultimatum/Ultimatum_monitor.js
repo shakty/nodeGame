679e3081060111b2749d733c8589b4a4fe62b951
philippkueng <git@philippkueng.ch>
2012-06-10 03:44:19 +0200

    added Payment widget to monitor; added depencies to html/js; still open -> payment increases after steps

--- games/ultimatum/Ultimatum_monitor.js
+++ games/ultimatum/Ultimatum_monitor.js
@@ -10,9 +10,52 @@ function Ultimatum_monitor () {
 	this.auto_step = false;
 	this.observer = true;
 	
+	window.App = Ember.Application.create();
+
 	this.init = function() {
 		node.window.setup('MONITOR');
+
+		// Create the Ember objects and bindings.
+		App.Player = DS.Model.extend({
+			id: DS.attr('string'),
+			balance: DS.attr('string')
+		});
+
+		App.players = Ember.ArrayController.create();
+
+		var paymentView = Ember.View.create({
+			templateName: 'PaymentWidget'
+		});
+		paymentView.appendTo('#root');
 	};
+
+	var players_ids = []; // List of player ids to check if a player object is new.
+
+	node.on('UPDATED_PLIST', function(){
+		var playerlist = node.game.pl.db;
+
+		// For each of those objects, is the user already in the system (Ember.store)?
+		if(playerlist.length >= 0){
+
+			App.players.set('content', playerlist);
+
+			_.each(playerlist, function(player){
+				if(!_.include(players_ids, player.id)){
+					players_ids.push(player.id);
+
+					// Add the initial amount to the players balance.
+					player.balance = 10;
+				}
+			});
+		}
+
+		// How to get the latest move? The player which caused the UPDATED_PLIST.
+		// Which object got updated?
+		// Listening to the socket.io commands? + add some timeout to make sure the playerlist has been updated?
+
+		console.log('updated'); // DEBUG
+		console.log(playerlist); // DEBUG
+	});
 	
 	function printGameState () {
 		var name = node.game.gameLoop.getName(node.state);
